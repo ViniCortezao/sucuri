f7a50ec9d1ed26a08241e9eeccc123ecaa53a32f
ViniCortezao <134319362+ViniCortezao@users.noreply.github.com>
2023-11-22 15:40:52 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Mythical Mastery - Login</title>
+  <link rel="stylesheet" type="text/css" href="login-style.css">
+</head>
+<body>
+<?php
+echo 'hello world';
+?>
+</body>
+</html>
